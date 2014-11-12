7c65d196458141a8e7bdeb10f62cb1d83e924767
s <s>
2014-11-12 09:38:21 +0800

    simplify dvb project copied from ldpc_awgn

--- win32/diy/ldpc_bch_dvb-s2.cpp
+++ win32/diy/ldpc_bch_dvb-s2.cpp
@@ -4,31 +4,20 @@
 using namespace std;
 using namespace itpp;
 
+#define		FILENAME_IT		"RU_1000.it"
+#define		EBNO			0.67
+
 int main(int argc, char **argv)
 {
-  if (argc < 2) {
-    it_info("Usage: " << argv[0] << " codec_file.it [EbN0_dB]");
-    return 1;
-  }
-
-  int64_t Nbits = 100000LL; // maximum number of bits simulated 5000000000LL
+  int64_t Nbits = 10000LL; // maximum number of bits simulated 5000000000LL
                                 // for each SNR point
   int Nbers = 2000;            // target number of bit errors per SNR point
   double BERmin = 1e-6;        // BER at which to terminate simulation
-  vec EbN0db = "0.6:0.2:5";
 
   LDPC_Generator_Systematic G; // for codes created with ldpc_gen_codes since generator exists
-  LDPC_Code C(argv[1], &G);
-  bool single_snr_mode = false;
-  if (argc == 3) {
-    istringstream ss(argv[2]);
-    double x;
-    ss >> x;
-    EbN0db = x;
-    single_snr_mode = true;
-  }
+  LDPC_Code C(FILENAME_IT, &G);
 
-  cout << "Running with Eb/N0: " << EbN0db << endl;
+  cout << "Running with Eb/N0: " << EBNO << endl;
 
   // High performance: 2500 iterations, high resolution LLR algebra
   C.set_exit_conditions(2500);
@@ -45,9 +34,9 @@ int main(int argc, char **argv)
 
   RNG_randomize();
 
-  for (int j = 0; j < length(EbN0db); j++) {
+
     // Noise variance is N0/2 per dimension
-    double N0 = pow(10.0, -EbN0db(j) / 10.0) / C.get_rate();
+    double N0 = pow(10.0, -EBNO / 10.0) / C.get_rate();
     AWGN_Channel chan(N0 / 2);
     BERC berc;  // Counters for coded and uncoded BER
     BLERC ferc; // Counter for coded FER
@@ -70,29 +59,15 @@ int main(int argc, char **argv)
       berc.count(bitsin, bitsout);
       ferc.count(bitsin, bitsout);
 
-      if (single_snr_mode) {
-        cout << "Eb/N0 = " << EbN0db(j) << "  Simulated "
+      
+        cout << "Eb/N0 = " << EBNO << "  Simulated "
              << ferc.get_total_blocks() << " frames and "
              << berc.get_total_bits() << " bits. "
              << "Obtained " << berc.get_errors() << " bit errors. "
              << " BER: " << berc.get_errorrate()
              << " FER: " << ferc.get_errorrate() << endl << flush;
-      }
-      else {
-        if (berc.get_errors() > Nbers)
-          break;
-      }
     }
 
-    cout << "Eb/N0 = " << EbN0db(j) << "  Simulated "
-         << ferc.get_total_blocks() << " frames and "
-         << berc.get_total_bits() << " bits. "
-         << "Obtained " << berc.get_errors() << " bit errors. "
-         << " BER: " << berc.get_errorrate()
-         << " FER: " << ferc.get_errorrate() << endl << flush;
-    if (berc.get_errorrate() < BERmin)
-      break;
-  }
 
   return 0;
 }
