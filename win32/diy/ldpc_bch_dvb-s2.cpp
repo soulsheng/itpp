4104201a08923df3acbdf01425253f3975e02a0c
s <s>
2014-11-13 16:18:47 +0800

    rename variable

--- win32/diy/ldpc_bch_dvb-s2.cpp
+++ win32/diy/ldpc_bch_dvb-s2.cpp
@@ -16,11 +16,11 @@ int main(int argc, char **argv)
 {
 
 	LDPC_Generator_Systematic G; // for codes created with ldpc_gen_codes since generator exists
-	LDPC_Code C(FILENAME_IT, &G);
-	cout << C << endl;
+	LDPC_Code ldpc(FILENAME_IT, &G);
+	cout << ldpc << endl;
 
 	// High performance: 2500 iterations, high resolution LLR algebra
-	C.set_exit_conditions(2500);
+	ldpc.set_exit_conditions(2500);
 
 	// Alternate high speed settings: 50 iterations, logmax approximation
 	// C.set_llrcalc(LLR_calc_unit(12,0,7));
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
 	BPSK Mod;
 
 	// Noise variance is N0/2 per dimension
-	double N0 = pow(10.0, -EBNO / 10.0) / C.get_rate();
+	double N0 = pow(10.0, -EBNO / 10.0) / ldpc.get_rate();
 	AWGN_Channel chan(N0 / 2);
 
 	BERC berc;  // Counters for coded and uncoded BER
@@ -40,9 +40,9 @@ int main(int argc, char **argv)
 	RNG_randomize();
 
 
-	int N = C.get_ninfo();             // number of bits per codeword
+	int kldpc = ldpc.get_ninfo();             // number of bits per codeword
 
-	int nSplit = N / N_BCH;
+	int nSplit = kldpc / N_BCH;
 	int Nbch = nSplit * N_BCH;
 
 	int Kbch = nSplit * K_BCH;
@@ -52,22 +52,23 @@ int main(int argc, char **argv)
     for (int64_t i = 0; i < COUNT_REPEAT; i ++) 
 	{
 
-		bvec bitsin = randb(Kbch);
+		bvec bitsinBCHEnc = randb(Kbch);
 
-		bvec bitsoutBCH(Nbch);
+		bvec bitsoutBCHEnc(Nbch);
 		for (int j = 0; j < nSplit; j++)
 		{
-			bvec bitsinBCH = bitsin.mid(j*K_BCH, K_BCH);
-			bvec encodedBCH = bch.encode(bitsinBCH);
-			bitsoutBCH.set_subvector(j*N_BCH, encodedBCH);
+			bvec bitsinBCHOne = bitsinBCHEnc.mid(j*K_BCH, K_BCH);
+			bvec encodedBCHOne = bch.encode(bitsinBCHOne);
+			bitsoutBCHEnc.set_subvector(j*N_BCH, encodedBCHOne);
 		}
 
-		bvec bitsinLDPC(N);
-		bitsinLDPC.set_subvector(0, bitsoutBCH);
 
-		bvec decbits = C.encode(bitsinLDPC);
+		bvec bitsinLDPCEnc(kldpc);
+		bitsinLDPCEnc.set_subvector(0, bitsoutBCHEnc);
+
+		bvec bitsoutLDPCEnc = ldpc.encode(bitsinLDPCEnc);
 		
-		vec s = Mod.modulate_bits(decbits);
+		vec s = Mod.modulate_bits(bitsoutLDPCEnc);
 
 		
 		// Received data
@@ -78,22 +79,22 @@ int main(int argc, char **argv)
 
 		// Decode the received bits
 		QLLRvec llr;
-		C.bp_decode(C.get_llrcalc().to_qllr(softbits), llr);
-		bvec bitsoutAll = llr < 0;
-		bvec bitsoutLDPC = bitsoutAll.left(Nbch);
+		ldpc.bp_decode(ldpc.get_llrcalc().to_qllr(softbits), llr);
+		bvec bitsoutLDPCDec = llr < 0;
+		bvec bitsinBCHDec = bitsoutLDPCDec.left(Nbch);
 		//      bvec bitsout = C.decode(softbits); // (only systematic bits)
 
-		bvec bitsDecodeBCH(Kbch);
+		bvec bitsoutBCHDec(Kbch);
 		for (int j = 0; j < nSplit; j++)
 		{
-			bvec bitsinBCH = bitsoutLDPC.mid(j*N_BCH, N_BCH);
-			bvec decodedBCH = bch.decode(bitsinBCH);
-			bitsDecodeBCH.set_subvector(j*K_BCH, decodedBCH);
+			bvec bitsinBCHDecOne = bitsinBCHDec.mid(j*N_BCH, N_BCH);
+			bvec bitsoutBCHDecOne = bch.decode(bitsinBCHDecOne);
+			bitsoutBCHDec.set_subvector(j*K_BCH, bitsoutBCHDecOne);
 		}
 
 		// Count the number of errors
-		berc.count(bitsin, bitsDecodeBCH);
-		ferc.count(bitsin, bitsDecodeBCH);
+		berc.count(bitsinBCHEnc, bitsoutBCHDec);
+		ferc.count(bitsinBCHEnc, bitsoutBCHDec);
 
       
 		cout << "Eb/N0 = " << EBNO << "  Simulated "
