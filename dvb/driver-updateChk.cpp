5f2713c510dfa1c2feeb5bc5bd1d5859c34a66e3
s <s>
2015-06-30 14:55:25 +0800

    timer kernel updateCheck, 0.14 ms on Test k20 for code 64800 32apsk 3/4

--- dvb/driver-updateChk.cpp
+++ dvb/driver-updateChk.cpp
@@ -1,6 +1,6 @@
 
 #include "driver-updateChk.cuh"
-
+#include "helper_timer.h"
 #include <cuda_runtime.h>
 #include <iostream>
 using	namespace	std;
@@ -10,10 +10,19 @@ void main()
 {
 	bool bStatus = false;
 
+	StopWatchInterface*	timer;
+	sdkCreateTimer( &timer );
+
 	driverUpdataChk ldpc;
+
+	sdkStartTimer( &timer );
 
 	if( !ldpc.launch() )
 		cout << "Failed to launch" << endl;
+
+	cudaDeviceSynchronize();
+	sdkStopTimer( &timer );
+	cout << "time of kernel updateCheck is : " << sdkGetTimerValue( &timer ) << endl;
 
 	if( !ldpc.verify() )
 		cout << "Failed to verify" << endl;
@@ -23,5 +32,6 @@ void main()
 	cudaDeviceReset();
 	system( "pause" );
 
+	sdkDeleteTimer( &timer );
 }
 
